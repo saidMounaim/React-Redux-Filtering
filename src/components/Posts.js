c9deef349a1cd3cd9fb97af9ec01c51c6381ee28
Said Mounaim <saidmounaim00@gmail.com>
2021-04-17 14:19:25 +0000

    Add Paginate

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -9,7 +9,7 @@ const Posts = () => {
 	const dispatch = useDispatch();
 	const [currentPage, setCurrentPage] = useState(1);
 
-	const { posts, loading, error } = useSelector((state) => state.PostReducers);
+	const { posts, loading } = useSelector((state) => state.PostReducers);
 
 	useEffect(() => {
 		dispatch(fetchPosts());
